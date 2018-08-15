79f4d6d477c80beb94982d63cb690debf727e4f0
Connor <mrconnorpakenham@gmail.com>
2018-08-15 09:59:14 +0100

    Init Commit
    Initial Commit

---
+++ bot-controller.py
@@ -0,0 +1,79 @@
+import discord
+import asyncio
+from splatoonschedule import get_updates
+
+TOKEN = 'XXXXXXXXX'
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if message.content.startswith('!sub'):
+        
+        user = message.author.mention
+        sublist = message.content.split(" ")[1]
+
+        f = open("{0}/{1}".format(sublist, sublist), "r") 
+        flag = False
+        
+        for line in f:
+            subscriber = line.split("//")[0]
+            if user == subscriber.strip():
+                flag = True
+
+        if flag == False:
+            try:
+                f = open("{0}/{1}".format(sublist, sublist), "a")
+                
+
+                f.write("{0}//{1}\n".format(user, sublist))
+
+                msg = "Subbed"
+
+            except OSError:
+                msg = "No such mailing list!"
+        else:
+            msg = "Already on the mailing list"
+
+        await client.send_message(message.channel, msg)
+
+async def my_background_task():
+    await client.wait_until_ready()
+    channel = discord.Object(id='478905754510688279')
+    while not client.is_closed:
+
+        update = get_updates()
+        f = open("splatoon2/lastmsg", "r")
+        oldupdate = f.read()
+        if(oldupdate != update):
+
+            msg = "Salmon Run" 
+            msg += "\n=================================\n"
+            msg +=  update
+            msg += "\n=================================\n"
+
+            sub_file = open("splatoon2/splatoon2", "r")
+
+            for line in sub_file:
+                subscriber,topic = line.split("//")
+                if 'splatoon2' == topic.strip():
+                    msg += subscriber + " "
+
+            await client.send_message(channel, msg)
+            f = open("splatoon2/lastmsg", "w")
+            f.write(update)
+
+        await asyncio.sleep(7200) # task runs every 2 hours
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+client.loop.create_task(my_background_task())
+client.run(TOKEN)
---
+++ splatoonschedule.py
@@ -0,0 +1,57 @@
+from lxml import html
+import requests
+import datetime
+
+def format_datetime(data):
+    date, time = data.split(" ")
+    new_time =convert_to_bst(time)
+
+    month, day = date.split("/")
+
+    if "am" in new_time and "pm" in time: 
+        day = int(day) + 1
+        if day > 31:
+            month = int(month) + 1
+            day = day - 31
+
+    return (time + " " + str(day) + "-" + str(month)+ "-" + str(datetime.datetime.now().year))
+ 
+    
+
+def convert_to_bst(time):
+    
+    if "am" in time: 
+        time = int(time.strip("am")) + 8
+    elif "pm" in time: 
+        time = int(time.strip("pm")) + 20
+    
+    if time < 12:
+        time = (str(time)+ "am")
+    elif time > 12   and time < 24 :
+        time = time - 12
+
+        time = (str(time)+ "pm")   
+    else:
+        time = time - 24
+        time = (str(time)+ "am")
+
+    return time
+
+def format_post(start, end, maps, weapons):
+    """
+    Formats the post to make it more human readable
+    """
+    return('Start Date: {0} \nEnd Date: {1} \nMap: {2} \nWeapons: {3}'.format(start, end, maps, weapons))
+
+def get_updates():
+    page = requests.get('https://twitter.com/grizzcoemployee?lang=en')
+    tree = html.fromstring(page.content)
+
+    tweet = tree.xpath('//p[@class="TweetTextSize TweetTextSize--normal js-tweet-text tweet-text"]/text()')[1]
+    parts = tweet.split(". ")
+
+    start,end = parts[0].split("-")
+
+    start = format_datetime(start)
+    end = format_datetime(end)
+    return (format_post(start, end, parts[1], parts[2]))
